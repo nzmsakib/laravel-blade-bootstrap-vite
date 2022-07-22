0498503cfe20b5d66e205342a5ab7a72b16cda8f
nzmsakib <engrsakibcse@outlook.com>
2022-07-22 11:47:20 +0600

    added jquery and custom js capabilities

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,6 @@ Route::get('/', function () {
     return redirect(route('home'));
 });
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
